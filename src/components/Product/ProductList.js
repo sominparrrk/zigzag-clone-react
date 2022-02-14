f7088597145571e5e439a6abc3fbac283bf1e9ec
sominparrrk <itsmechloe@naver.com>
2022-02-15 05:06:06 +0900

    Design: Add lower z-index to product list

--- src/components/Product/ProductList.js
+++ src/components/Product/ProductList.js
@@ -4,7 +4,7 @@ import ProductItem from './ProductItem';
 
 const ProductList = () => {
   return (
-    <div className='product__list grid grid-cols-2 gap-3 py-2'>
+    <div className='product__list z-0 grid grid-cols-2 gap-3 py-2'>
       {ProductData.map((product, i) => (
         <ProductItem key={product.id} index={i + 1} product={product} />
       ))}

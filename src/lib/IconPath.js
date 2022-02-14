8a9e840208bd5aff5b966aff4bd6310277e4d101
sominparrrk <itsmechloe@naver.com>
2022-02-15 00:39:15 +0900

    Feat: Add static icon path

---
+++ src/lib/IconPath.js
@@ -0,0 +1,10 @@
+export const IconPath = {
+  basket: '/icons/basket.svg',
+  groupView: '/icons/groupView.svg',
+  likeList: '/icons/likeList.svg',
+  like: '/icons/like.svg',
+  myPage: '/icons/myPage.svg',
+  search: '/icons/search.svg',
+  shoppingMenu: '/icons/shoppingMenu.svg',
+  zHome: '/icons/zHome.svg',
+};

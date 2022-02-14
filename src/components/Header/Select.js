ce76517d3013c8bd192acaf507cc56291fd53174
sominparrrk <itsmechloe@naver.com>
2022-02-15 00:41:56 +0900

    Feat: Add select option bar in header

---
+++ src/components/Header/Select.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+const Select = () => {
+  return (
+    <div className='personal__select__area flex justify-between my-2'>
+      <div className='age__area'>
+        <button className='border rounded-3xl border-gray-300 px-3 py-2 text-xs'>
+          전체
+        </button>
+        <button className='border rounded-3xl border-gray-300 px-3 py-2 text-xs'>
+          10대
+        </button>
+        <button className='border rounded-3xl border-gray-300 px-3 py-2 text-xs'>
+          20대
+        </button>
+        <button className='bg-gray-900 text-white rounded-3xl px-3 py-2 text-xs'>
+          30대
+        </button>
+      </div>
+      <select
+        name='choice'
+        defaultValue='daily'
+        className='date__basis__area text-xs'
+      >
+        <option value='daily'>데일리</option>
+        <option value='weekly'>위클리</option>
+      </select>
+    </div>
+  );
+};
+
+export default Select;
